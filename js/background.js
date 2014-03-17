c63fbc0141545367bbcb0c128b2f92477be0a4d5
Maxim <c4d3r@hotmail.com>
2014-03-17 11:37:13 +0100

    change colors

--- js/background.js
+++ js/background.js
@@ -1,52 +1,18 @@
 /**
  * Created by Maxim on 12/03/14.
  */
-var _document = document;
 
-var canvas = _document.getElementById("mainCanvas");
-var ctx = canvas.getContext("2d");
+function Cell(colors, x, y) {
 
-var wH = window.innerHeight;
-var wW = window.innerWidth;
-canvas.setAttribute("height", wH);
-canvas.setAttribute("width", wW);
+    this.colors2 = ["#D45D5D", "#ED8D3E", "#FF7AC8"];
+    this.currentColor = "#000";
 
-console.log(wW);
-console.log(wH);
-
-var colors = ["#915DD4", "#97D45D"];
-var colors2 = ["#5DA4D4", "#D45D5D", "#ED8D3E", "#FF7AC8"];
-
-var amountHorizontal = 400;
-
-var sizeSquare = wW / amountHorizontal;
-
-// horizontal 1
-var objMatrix = new Matrix(amountHorizontal, amountHorizontal);
-objMatrix.init();
-
-//fill matrix
-var milliseconds1 = (new Date).getTime();
-for(var i = 0; i < amountHorizontal; i++) {
-    //fill vertical for this row
-    for(var j = 0; j < amountHorizontal; j++) {
-        if(i % 2 == 0 && (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && (i % 2 == 1 && j % 2 == 1))) {
-            objMatrix.addCell(new Cell(colors[Math.round(Math.random() * 3)], i, j));
-        } else {
-            objMatrix.addCell(new Cell(colors2[Math.round(Math.random() * 3)], i, j));
-        }
-    }
-}
-var milliseconds2 = (new Date).getTime();
-console.log("done: " + (milliseconds2 - milliseconds1));
-objMatrix.display();
-var milliseconds3 = (new Date).getTime();
-console.log("done: " + (milliseconds3 - milliseconds2));
-function Cell(color, x, y) {
-
-    this.color = color;
     this.x = x;
     this.y = y;
+
+
+    this.path = new Path.Rectangle(new Point(i * sizeSquare, j * sizeSquare), new Size(sizeSquare, sizeSquare));
+    this.copy = path.clone();
 
     this.hasTop = function() {
         return y - 1 >= 0;
@@ -63,6 +29,17 @@ function Cell(color, x, y) {
     this.hasBottom = function() {
         return y + 1 < Math.ceil(sizeSquare);
     }
+    this.draw = function() {
+        this.copy.fillColor = this.currentColor;
+    }
+}
+Cell.prototype.changeColors = function(colors) {
+    this.currentColor = colors[Math.round(Math.random() * (colors.length - 1))];
+    this.copy.fillColor = this.currentColor;
+}
+Cell.prototype.startColorChange = function() {
+    var self = this;
+    loadingTimer = setInterval(function() { self.changeColors(["#D45D5D", "#ED8D3E", "#FF7AC8"]); }, 100);
 }
 
 function Matrix(rows, cols) {
@@ -98,18 +75,58 @@ function Matrix(rows, cols) {
     this.display = function() {
         for(var i = 0; i < this.cells.length; i++)
         {
-            var path = new Path.Rectangle(new Point(i * sizeSquare, 0), new Size(sizeSquare, sizeSquare));
-
-            var copy = path.clone();
-            copy.fillColor  = this.cells[i][0].color;
+            this.cells[i][0].draw();
+            this.cells[i][0].startColorChange();
 
             for(var j = 1; j < this.cells[i].length; j++) {
 
-                var pathJ = new Path.Rectangle(new Point(i * sizeSquare, j * sizeSquare), new Size(sizeSquare, sizeSquare));
+                this.cells[i][j].draw();
+                this.cells[i][j].startColorChange();
 
-                var copyJ = pathJ.clone();
-                copyJ.fillColor  = this.cells[i][j].color;
             }
         }
     }
 }
+
+
+var _document = document;
+
+var canvas = _document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
+
+var wH = window.innerHeight;
+var wW = window.innerWidth;
+canvas.setAttribute("height", wH);
+canvas.setAttribute("width", wW);
+
+console.log(wW);
+console.log(wH);
+
+//var colors = ["#915DD4", "#97D45D"];
+//var colors2 = ["#5DA4D4", "#D45D5D", "#ED8D3E", "#FF7AC8"];
+var colors = ["#97D45D", "#FFF"];
+var colors2 = ["#D45D5D", "#ED8D3E", "#FF7AC8"];
+var amountHorizontal = 40;
+
+var sizeSquare = wW / amountHorizontal;
+
+// horizontal 1
+var objMatrix = new Matrix(amountHorizontal, amountHorizontal);
+objMatrix.init();
+
+//fill matrix
+for(var i = 0; i < amountHorizontal; i++) {
+    //fill vertical for this row
+    for(var j = 0; j < amountHorizontal; j++) {
+        if(i % 2 == 0 && (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && (i % 2 == 1 && j % 2 == 1))) {
+            objMatrix.addCell(new Cell(colors, i, j));
+        } else {
+            objMatrix.addCell(new Cell(colors2, i, j));
+        }
+    }
+}
+objMatrix.display();
+
+
+
+
